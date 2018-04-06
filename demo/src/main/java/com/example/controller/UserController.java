99b5af4eb0a0b797315c2be94459533eed643cfd
katragadda.harika123@gmail.com <katragadda.harika123@gmail.com>
2018-04-06 00:16:36 -0400

    updating userviews page

--- demo/src/main/java/com/example/controller/UserController.java
+++ demo/src/main/java/com/example/controller/UserController.java
@@ -1,17 +1,26 @@
 package com.example.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.example.model.Product;
+import com.example.service.ProductService;
 @Controller
 public class UserController {
+	
+	@Autowired
+	private ProductService productService;
+	
 	@RequestMapping(value="/userpages/userview", method = RequestMethod.GET)
 	public ModelAndView userproducts(){
-		ModelAndView modelAndView = new ModelAndView("redirect:/userpages/userview");
-		System.out.println("in the contrlooer**");
-/*		Product product = new Product();
-		modelAndView.addObject("product", product);*/
+		ModelAndView modelAndView = new ModelAndView();
+		List<Product> listProducts = productService.listProducts();
+		modelAndView.addObject("listProducts" , listProducts);
 		modelAndView.setViewName("/userpages/userview");
 		return modelAndView;
 	}
